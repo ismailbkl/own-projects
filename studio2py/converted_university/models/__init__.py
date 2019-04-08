d64f0f71df30ad6164f17cc543f6988b2a9999ef
ismailbkl <mrismailelbakkali@gmail.com>
2019-04-08 21:45:02 +0100

    [Fix] modules

--- studio2py/converted_university/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-# (C) 2019 Smile (<http://www.smile.eu>)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from . import x_departments 
-from . import x_classrooms 
-from . import x_proffessors 
-from . import x_subjects 
-from . import x_students 
